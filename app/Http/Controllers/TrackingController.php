23d2754ea5470b204efcc3bdb3327032d1759e58
jamiemrobison <ogunbojoshua@gmail.com>
2020-01-09 18:54:22 +0100

    Update variable name

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -8,10 +8,10 @@ use App\Http\Requests\Tracking\RetrieveDetailsRequest;
 
 class TrackingController extends Controller
 {
-    public $tracking = null;
+    public $trackingRepository = null;
 
     public function construct(TrackingRepositoryInterface $trackingRepository) {
-        $this->tracking = $trackingRepository;
+        $this->trackingRepository = $trackingRepository;
     }
 
     /**
@@ -21,9 +21,8 @@ class TrackingController extends Controller
      * @param RetrieveDetailsRequest $request
      * @return array
      */
-
     public function details(RetrieveDetailsRequest $request) : array
     {
-        return [];        //$this->tracking->findByCode($request->trackingCode);
+        return [];        //$this->trackingRepository->findByCode($request->trackingCode);
     }
 }
